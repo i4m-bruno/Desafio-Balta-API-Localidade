e1d26d92a2561fc42f665f54684b930aba645271
Bruno Cardoso <ossBruno@outlook.com>
2023-10-11 13:41:31 -0300

    [feat] teste-email-invalido

--- DesafioLocalidade.Test/UsuarioTestes.cs
+++ DesafioLocalidade.Test/UsuarioTestes.cs
@@ -9,7 +9,7 @@ public class IdentityServiceTests
 #pragma warning disable CS8625 // Não é possível converter um literal nulo em um tipo de referência não anulável.
 
     [Fact]
-    public async Task CadastrarUsuario_ShouldReturnSuccess_WhenUserIsCreatedSuccessfully()
+    public async Task CadastrarUsuario_SucessoEsperado()
     {
         // Arrange
         var mockUserManager = new Mock<UserManager<IdentityUser>>(
@@ -37,7 +37,7 @@ public class IdentityServiceTests
     }
 
     [Fact]
-    public async Task CadastrarUsuario_ShouldReturnErrors_WhenUserCreationFails()
+    public async Task CadastrarUsuario_ErroEsperado_SenhaNaoConfere()
     {
         // Arrange
         var mockUserManager = new Mock<UserManager<IdentityUser>>(
@@ -51,6 +51,34 @@ public class IdentityServiceTests
         var service = new IdentityService(mockSignInManager.Object, mockUserManager.Object);
 
         var usuarioViewModel = new UsuarioViewModel("teste@email.com", "Senha@123", "Senha@1234");
+
+        mockUserManager
+            .Setup(um => um.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Failed(new IdentityError { Description = "User creation failed" }));
+
+        // Act
+        var result = await service.CadastrarUsuario(usuarioViewModel);
+
+        // Assert
+        Assert.False(result.Sucesso);
+        Assert.True(result.Erros.Count >= 1);
+    }
+    
+    [Fact]
+    public async Task CadastrarUsuario_ErroEsperado_EmailInvalido()
+    {
+        // Arrange
+        var mockUserManager = new Mock<UserManager<IdentityUser>>(
+            Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
+
+        var mockSignInManager = new Mock<SignInManager<IdentityUser>>(
+            mockUserManager.Object,
+            Mock.Of<IHttpContextAccessor>(),
+            Mock.Of<IUserClaimsPrincipalFactory<IdentityUser>>(), null, null, null, null);
+
+        var service = new IdentityService(mockSignInManager.Object, mockUserManager.Object);
+
+        var usuarioViewModel = new UsuarioViewModel("testeemail.com", "Senha@123", "Senha@123");
 
         mockUserManager
             .Setup(um => um.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
