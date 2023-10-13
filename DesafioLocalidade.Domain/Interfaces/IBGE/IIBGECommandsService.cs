4119a850ca19f55d9cec70665cdb52eda0f432f9
Bruno Cardoso <ossBruno@outlook.com>
2023-10-13 19:41:30 -0300

    feat

---
+++ DesafioLocalidade.Domain/Interfaces/IBGE/IIBGECommandsService.cs
@@ -0,0 +1,12 @@
+﻿using DesafioLocalidade.Domain.ViewModels;
+
+namespace DesafioLocalidade.Domain.Interfaces.IBGE
+{
+    public interface IIBGECommandsService
+    {
+        Task<IBGEViewModel> Create();
+        Task<IBGEViewModel> Update();
+        Task<bool> Delete();
+        Task<IEnumerable<IBGEViewModel>> ExcelUpdload();
+    }
+}
